3b9dfef1bc994999f98907a36030b6a5f5c8e642
arameniang <53411160+arameniang@users.noreply.github.com>
2019-10-05 03:04:49 -0400

    Script on variables

---
+++ BasicMath.r
@@ -0,0 +1,24 @@
+"Notes = R is a case sensitive"
+require(ggplot2)
+require(coefplot)
+1+1
+3*4
+4/2
+4/3
+  
+X<-2
+Y=5
+Z=X*Y
+A=B=7
+A<-4
+assign("J",4)
+
+
+"Remove a variable"
+remove(J)
+rm(Z)
+assign("J",4)
+Z=X*Y
+
+"Last video watched is 8"
+
